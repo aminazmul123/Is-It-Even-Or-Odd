9fea17e5f4e099db2221525a59dfc93bcc359042
Nazmul Hoque <110515748+aminazmul123@users.noreply.github.com>
2023-05-29 00:49:33 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let number = parseInt(prompt("Enter any number, and I'll tell you if it's even or odd:"));
+
+
+if (number%2 == 0) {
+ document.write("Even Number");
+}
+else{
+ document.write("Odd Number");
+}
